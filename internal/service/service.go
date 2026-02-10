ed5e21f88f6d0963d49dec38ce965a019399f1b3
leezyarch <191919qaz@gmail.com>
2026-02-11 00:56:22 +0300

    add issue service

--- internal/service/service.go
+++ internal/service/service.go
@@ -2,7 +2,7 @@ package service
 
 import (
     "context"
-
+	"errors"
     "Go-IssueTracker-API/internal/model"
     "Go-IssueTracker-API/internal/repository"
 )
@@ -24,21 +24,27 @@ func NewIssueService(repo repository.IssueRepository) *IssueService {
 }
 
 func (s *IssueService) CreateIssue(ctx context.Context, issue *model.Issue) (int, error) {
+	if issue.Title == "" {
+		return 0, errors.New("title is required")
+	}
 
+	issue.Status = "open"
+
+	return s.repo.CreateIssue(ctx, issue)
 }
 
 func (s *IssueService) GetIssueByID(ctx context.Context, id int) (*model.Issue, error) {
-
+	return s.repo.GetIssueByID(ctx, id)
 }
 
 func (s *IssueService) UpdateIssue(ctx context.Context, issue *model.Issue) error {
-
+	return s.repo.UpdateIssue(ctx, issue)
 }
 
 func (s *IssueService) DeleteIssue(ctx context.Context, id int) error {
-
+	return s.repo.DeleteIssue(ctx, id)
 }
 
 func (s *IssueService) ListIssues(ctx context.Context) ([]*model.Issue, error) {
-	
+	return s.repo.ListIssues(ctx)
 }
