4cb80417f3a7dd3a6aef049248685c08737b00be
leezyarch <191919qaz@gmail.com>
2026-02-13 01:13:13 +0300

    add testes for service

---
+++ internal/service/service_test.go
@@ -0,0 +1,184 @@
+package service_test
+
+import (
+	"Go-IssueTracker-API/internal/model"
+	"Go-IssueTracker-API/internal/service"
+	"context"
+	"testing"
+)
+
+type MockRepo struct {
+	CreateFunc     func(ctx context.Context, issue *model.Issue) (int, error)
+	GetByIDFunc    func(ctx context.Context, id int) (*model.Issue, error)
+	UpdateFunc     func(ctx context.Context, issue *model.Issue) error
+	DeleteFunc     func(ctx context.Context, id int) error
+	ListFunc       func(ctx context.Context) ([]*model.Issue, error)
+}
+
+func (m *MockRepo) CreateIssue(ctx context.Context, issue *model.Issue) (int, error) {
+	return m.CreateFunc(ctx, issue)
+}
+
+func (m *MockRepo) GetIssueByID(ctx context.Context, id int) (*model.Issue, error) {
+	return m.GetByIDFunc(ctx, id)
+}
+
+func (m *MockRepo) UpdateIssue(ctx context.Context, issue *model.Issue) error {
+	return m.UpdateFunc(ctx, issue)
+}
+
+func (m *MockRepo) DeleteIssue(ctx context.Context, id int) error {
+	return m.DeleteFunc(ctx, id)
+}
+
+func (m *MockRepo) ListIssues(ctx context.Context) ([]*model.Issue, error) {
+	return m.ListFunc(ctx)
+}
+
+func TestCreateIssue(t *testing.T) {
+    mockRepo := &MockRepo{
+        CreateFunc: func(ctx context.Context, issue *model.Issue) (int, error) {
+            return 1, nil
+        },
+    }
+
+    service := service.NewIssueService(mockRepo)
+
+	issue := &model.Issue{
+		Title:       "Test Issue",
+		Description: "This is a test issue",
+		Status:      "open",
+	}
+
+	_, err := service.CreateIssue(context.Background(), issue)
+
+    if err != nil {
+        t.Fatalf("expected no error, got %v", err)
+    }
+}
+
+func TestCreateIssue_InvalidStatus(t *testing.T) {
+	mockRepo := &MockRepo{
+        CreateFunc: func(ctx context.Context, issue *model.Issue) (int, error) {
+            return 1, nil
+        },
+    }
+
+    service := service.NewIssueService(mockRepo)
+
+	issue := &model.Issue{
+		Title:       "Test Issue",
+		Description: "This is a test issue",
+		Status:      "123",
+	}
+
+	_, err := service.CreateIssue(context.Background(), issue)
+
+	if err != nil {
+		t.Fatalf("expected error, got %v", err)
+	}
+}
+
+func TestGetIssueByID_NotFound(t *testing.T) {
+	mockRepo := &MockRepo{
+		GetByIDFunc: func(ctx context.Context, id int) (*model.Issue, error) {
+			return nil, nil
+		},
+	}
+
+	service := service.NewIssueService(mockRepo)
+
+	issue := &model.Issue{
+		ID: 1,
+		Title: "test",
+	}
+
+	_, err := service.GetIssueByID(context.Background(), issue.ID)
+
+	if err != nil {
+		t.Fatalf("expected no error, got %v", err)
+	}
+}
+
+func TestUpdateIssue(t *testing.T) {
+	mockRepo := &MockRepo{
+		UpdateFunc: func(ctx context.Context, issue *model.Issue) (error) {
+			return nil
+		},
+	}
+
+	service := service.NewIssueService(mockRepo)
+
+	issue := &model.Issue{
+		Title:       "Test Issue",
+		Description: "This is a test issue",
+		Status:      "open",
+	}
+
+	err := service.UpdateIssue(context.Background(), issue)
+
+	if err != nil {
+		t.Fatalf("expected no error, got %v", err)
+	}
+}
+
+func TestUpdateIssue_InvalidStatus(t *testing.T) {
+	mockRepo := &MockRepo{
+		UpdateFunc: func(ctx context.Context, issue *model.Issue) (error) {
+			return nil
+		},
+	}
+
+	service := service.NewIssueService(mockRepo)
+
+	issue := &model.Issue{
+		Title:       "Test Issue",
+		Description: "This is a test issue",
+		Status:      "123",
+	}
+
+	err := service.UpdateIssue(context.Background(), issue)
+
+	if err == nil {
+		t.Fatalf("expected error, got %v", err)
+	}
+}
+
+func TestDeleteIssue(t *testing.T) {
+	mockRepo := &MockRepo{
+		DeleteFunc: func(ctx context.Context, id int) error {
+			return nil
+		},
+	}
+
+	service := service.NewIssueService(mockRepo)
+
+	err := service.DeleteIssue(context.Background(), 1)
+
+	if err != nil {
+		t.Fatalf("expected no error, got %v", err)
+	}
+}
+
+func TestListIssues(t *testing.T) {
+	mockRepo := &MockRepo{
+		ListFunc: func(ctx context.Context) ([]*model.Issue, error) {
+			return []*model.Issue{
+				{ID: 1, Title: "Test Issue 1"},
+				{ID: 2, Title: "Test Issue 2"},
+			}, nil
+		},
+	}
+
+	service := service.NewIssueService(mockRepo)
+
+	issues, err := service.ListIssues(context.Background())
+
+	if err != nil {
+		t.Fatalf("expected no error, got %v", err)
+	}
+
+	if len(issues) != 2 {
+		t.Fatalf("expected 2 issues, got %d", len(issues))
+	}
+}
